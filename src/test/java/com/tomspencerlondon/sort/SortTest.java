f0866fc9a1620fbb19738ec7e5169728e7bc4b8f
Tom Spencer <thomas.spencer@codurance.com>
2022-09-13 19:52:52 +0100

    Add middles

--- src/test/java/com/tomspencerlondon/sort/SortTest.java
+++ src/test/java/com/tomspencerlondon/sort/SortTest.java
@@ -22,6 +22,7 @@ public class SortTest {
     assertEquals(asList(1, 2, 3), sort(asList(3, 2, 1)));
     assertEquals(asList(1, 2, 3, 4), sort(asList(1,2, 3, 4)));
     assertEquals(asList(1, 2, 3, 4, 5), sort(asList(1,2,5, 3, 4)));
+    assertEquals(asList(1, 1, 2, 3), sort(asList(1, 3, 1, 2)));
   }
 
   private List<Integer> sort(List<Integer> list) {
@@ -29,11 +30,12 @@ public class SortTest {
       return list;
     } else {
       int middle = list.get(0);
+      List<Integer> middles = list.stream().filter(x -> x == middle).collect(Collectors.toList());
       List<Integer> lessers = list.stream().filter(x -> x < middle).collect(Collectors.toList());
       List<Integer> greaters = list.stream().filter(x -> x > middle).collect(Collectors.toList());
       List<Integer> result = new ArrayList<>();
       result.addAll(sort(lessers));
-      result.add(middle);
+      result.addAll(middles);
       result.addAll(sort(greaters));
       return result;
     }
