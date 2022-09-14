4291b018094a5b4a3a66c6035ee5b5193af80503
Tom Spencer <thomas.spencer@codurance.com>
2022-09-14 09:06:08 +0100

    Complete QuickSort implementation

--- src/test/java/com/tomspencerlondon/sort/SortTest.java
+++ src/test/java/com/tomspencerlondon/sort/SortTest.java
@@ -23,17 +23,20 @@ public class SortTest {
     assertEquals(asList(1, 2, 3, 4), sort(asList(1,2, 3, 4)));
     assertEquals(asList(1, 2, 3, 4, 5), sort(asList(1,2,5, 3, 4)));
     assertEquals(asList(1, 1, 2, 3), sort(asList(1, 3, 1, 2)));
+    assertEquals(
+        asList(1, 1, 2, 3, 3, 3, 4, 5, 5, 6, 7, 8, 9, 9, 9),
+        sort(asList(3,1, 4, 1, 5, 9, 2, 6, 5, 3, 8, 9, 7, 9, 3)));
   }
 
   private List<Integer> sort(List<Integer> list) {
-    if (list.size() <= 1) {
-      return list;
+    List<Integer> result = new ArrayList<>();
+    if (list.size() == 0) {
+      return result;
     } else {
       int middle = list.get(0);
       List<Integer> middles = list.stream().filter(x -> x == middle).collect(Collectors.toList());
       List<Integer> lessers = list.stream().filter(x -> x < middle).collect(Collectors.toList());
       List<Integer> greaters = list.stream().filter(x -> x > middle).collect(Collectors.toList());
-      List<Integer> result = new ArrayList<>();
       result.addAll(sort(lessers));
       result.addAll(middles);
       result.addAll(sort(greaters));
